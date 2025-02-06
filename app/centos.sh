5271b878b4b392abf115918d51922116dd66ef49
unoMato <nomato@meiliboxi.fi>
2025-02-06 01:08:01 +0000

    java to headless and add java 21 for CentOS 10

--- app/centos.sh
+++ app/centos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo dnf update -y
 sudo dnf group install "Development tools" -y
-sudo dnf install git python3 python3-devel java-17-openjdk java-17-openjdk-devel -y
+sudo dnf install git python3 python3-devel java-17-openjdk-headless java-21-openjdk-headless  -y
 sudo useradd crafty -s /bin/bash
