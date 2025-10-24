4b84348d4a6f4732c31cd446c5be1a376db08e14
Zedifus <zedifus@zediverse.gg>
2025-10-24 20:34:28 +0100

    Remove JRE as JDK contains JRE
    This resolves the installation on Arch failing from package conflicts. Only JDK is required.

--- app/arch.sh
+++ app/arch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -Syu
-sudo pacman -S --noconfirm git python python-pip jre-openjdk jdk-openjdk rust
+sudo pacman -S --noconfirm git python python-pip jdk-openjdk rust
 install_return=$?
 if [[ "$install_return" != 0 ]];then
 	exit $install_return
