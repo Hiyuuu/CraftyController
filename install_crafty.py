e0aad4f9eb1cbef089be7c7a19034cd951442884
Noah Halstead <nhalstead00@gmail.com>
2026-03-18 09:35:45 -0400

    fix update_crafty.sh to use correct logic

--- install_crafty.py
+++ install_crafty.py
@@ -336,39 +336,40 @@ def make_update_script(target_directory: pathlib.Path):
     os.chdir(target_directory)
     logger.info("Changing to %s", os.path.abspath(os.curdir))
 
-    txt = "#!/bin/bash\n"
-    txt += f"cd {target_directory}\n"
-    txt += "source .venv/bin/activate \n"
-    txt += "cd crafty-4 \n"
-    txt += "\n"
-    txt += "if [[ -v 1 ]]; then\n"
-    txt += '    yn="$1"\n'
-    txt += "fi\n"
-    txt += "\n"
-    txt += "while true; do\n"
-    txt += "    if [[ ! -v yn ]]; then\n"
-    txt += "        read -p 'Can we overwrite any local codebase changes? (Y/N)' yn\n"
-    txt += "    fi\n"
-    txt += "    \n"
-    txt += "    case $yn in\n"
-    txt += "        [yY] | -y )\n"
-    txt += "            git reset --hard origin/master\n"
-    txt += "            break;;\n"
-    txt += "        [nN] | -n )\n"
-    txt += "            break;;\n"
-    txt += "        * )\n"
-    txt += "            unset yn\n"
-    txt += "            echo 'Please use Y or N to reply.';;\n"
-    txt += "    esac\n"
-    txt += "done\n"
-    txt += "\n"
-    txt += "git pull \n"
-    txt += "python3 -m ensurepip --upgrade \n"
-    txt += "pip3 install --upgrade pip --no-cache-dir\n"
-    txt += "pip3 install -r requirements.txt --no-cache-dir \n"
-    with open("update_crafty.sh", "w", encoding="utf-8") as update_crafty_sh_file:
-        update_crafty_sh_file.write(txt)
-        update_crafty_sh_file.close()
+    txt = fr"""#!/bin/bash
+    cd {target_directory}
+    source .venv/bin/activate 
+    cd crafty-4 
+
+    if [[ -n "$1" ]]; then
+        yn="$1"
+    fi
+
+    while true; do
+        if [[ -z "$yn" ]]; then
+            read -p 'Can we overwrite any local codebase changes? (Y/N) ' yn
+        fi
+        
+        case "$yn" in
+            [yY]* | -y )
+                git reset --hard origin/master
+                break ;;
+            [nN]* | -n )
+                break ;;
+            * )
+                unset yn
+                echo 'Please use Y or N to reply.' ;;
+        esac
+    done
+
+    git pull 
+    python3 -m ensurepip --upgrade 
+    pip3 install --upgrade pip --no-cache-dir
+    pip3 install -r requirements.txt --no-cache-dir
+    """
+
+    with open("update_crafty.sh", "w", encoding="utf-8") as f:
+        f.write(txt)
 
     helper.chmod_add_exec(pathlib.Path("update_crafty.sh"))
 
